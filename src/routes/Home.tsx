9b9e4901a6dd749f0558aff0d000a91a420e4d63
acecic82 <102938al@naver.com>
2024-08-25 05:39:34 +0900

    Reponseive Image add

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,42 +1,107 @@
-import { Box, Grid, Image, VStack, Text, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Image,
+  VStack,
+  Text,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
 import React from "react";
-import { FaStar } from "react-icons/fa";
+import { FaRegHeart, FaStar } from "react-icons/fa";
 
 export default function Home() {
   return (
     <Grid
       mt={10}
-      px={40}
+      px={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
       columnGap={4}
       rowGap={8}
-      templateColumns={"repeat(5, 1fr)"}
+      templateColumns={{
+        base: "1fr",
+        lg: "repeat(5, 1fr)",
+      }}
     >
-      <VStack alignItems={"flex-start"}>
-        <Box overflow={"hidden"} rounded="3xl" mb={3}>
-          <Image
-            // h="280"
-            src="https://www.coralmagazine.com/wp-content/uploads/2023/02/Paracanthurus-hepatus-1-of-1-14.jpg"
-          />
-        </Box>
-        <Box>
-          <Grid gap={1} templateColumns={"6fr 1fr"}>
-            <Text as="b" noOfLines={1} fontSize="md">
-              jasdlkfjlksadjlkfjklsadjflkjskdaljflkjaslk
-              djfkljasdklfjklajsdklfjklas
+      {[
+        1,
+        23,
+        12,
+        3,
+        21,
+        3,
+        213,
+        12,
+        ,
+        1,
+        1,
+        11,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+      ].map((index) => (
+        <VStack key={index} alignItems={"flex-start"}>
+          <Box position={"relative"} overflow={"hidden"} rounded="2xl" mb={3}>
+            <Image
+              // h="280"
+              src="https://www.coralmagazine.com/wp-content/uploads/2023/02/Paracanthurus-hepatus-1-of-1-14.jpg"
+            />
+            <Button
+              variant={"unstyled"}
+              position={"absolute"}
+              color="white"
+              top={0}
+              right={0}
+            >
+              <FaRegHeart size="20px" />
+            </Button>
+          </Box>
+          <Box>
+            <Grid gap={1} templateColumns={"6fr 1fr"}>
+              <Text as="b" noOfLines={1} fontSize="md">
+                jasdlkfjlksadjlkfjklsadjflkjskdaljflkjaslk
+                djfkljasdklfjklajsdklfjklas
+              </Text>
+              <HStack spacing={1}>
+                <FaStar size={15} />
+                <Text>5.0</Text>
+              </HStack>
+            </Grid>
+            <Text fontSize={"sm"} color="gray.600">
+              Seoul, S, Korea
             </Text>
-            <HStack spacing={1}>
-              <FaStar size={15} />
-              <Text>5.0</Text>
-            </HStack>
-          </Grid>
+          </Box>
           <Text fontSize={"sm"} color="gray.600">
-            Seoul, S, Korea
+            <Text as="b">$72</Text> / night
           </Text>
-        </Box>
-        <Text fontSize={"sm"} color="gray.600">
-          <Text as="b">$72</Text> / night
-        </Text>
-      </VStack>
+        </VStack>
+      ))}
     </Grid>
   );
 }
